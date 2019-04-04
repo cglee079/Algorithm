db696dabbc7b33f531abf7b6c5c64759cdb26a89
이찬구 <@DESKTOP-Q3AA9BG>
2019-04-04 22:01:04 +0900

    update

--- src/com/baekjoon/q02921/Main.java
+++ src/com/baekjoon/q02921/Main.java
@@ -1,17 +1,17 @@
-package com.baekjoon.q02921;
-
-import java.util.Scanner;
-
-public class Main {
-	public void run() {
-		Scanner scan = new Scanner(System.in);
-
-		int n = scan.nextInt();
-		
-		
-
-	}
-	public static void main(String[] args) {
-		new Main().run();
-	}
-}
+//package com.baekjoon.q02921;
+//
+//import java.util.Scanner;
+//
+//public class Main {
+//	public void run() {
+//		Scanner scan = new Scanner(System.in);
+//
+//		int n = scan.nextInt();
+//		
+//		for(int i = 0;)
+//
+//	}
+//	public static void main(String[] args) {
+//		new Main().run();
+//	}
+//}
